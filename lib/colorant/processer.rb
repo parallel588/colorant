7c6667b9f05bc178464697107600e02c295affdd
Maxim Pechnikov <parallel588@gmail.com>
2016-06-10 09:24:07 +0300

    Update processer.rb

--- lib/colorant/processer.rb
+++ lib/colorant/processer.rb
@@ -1,23 +1,23 @@
 module Colorant
   class Processer
-    attr_reader :colors, :depth, :file, :data
+    attr_reader :colors, :depth, :file, :data, :quantize, :crop
 
     def initialize(file, options = {})
-      begin
-        File.open(file)
-      rescue=>e
-        raise ArgumentError.new "#{file} does not exist or is not a valid image file"
-      end
       @file = file
       @colors = options[:colors] ? options[:colors].to_i : 8
       @depth = options[:depth] ? options[:depth].to_i : 16
-      @reporter_options = {:reporter => options[:reporter],
-                           :extended => options[:extended]}
+      @quantize = options[:quantize] ? options[:quantize] : 'YIQ'
+      @crop = options.fetch(:crop) { '80x60%' }
+
+      @reporter_options = {
+        reporter: options[:reporter],
+        extended: options[:extended]
+      }
       @data = []
     end
 
     def process!
-      raw_data = `convert #{file} -gravity Center -crop 80x60% +repage -format %c -colors #{colors} -depth #{depth} histogram:info:- | sort -r -k 1`
+      raw_data = `convert #{file.shellescape} -gravity Center -crop #{crop} +repage -format %c -dither None -quantize #{quantize}  -colors #{colors.to_i} -depth #{depth.to_i} histogram:info:- | sort -r -k 1`
       @data = Parser.parse(raw_data)
       Reporter.report(@data, @reporter_options)
     end
